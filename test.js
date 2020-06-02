66655388f5f29fb6478b5ff7f97a5f4436f7c6f8
Milo123459 <milo5huntington@gmail.com>
2020-06-02 16:32:05 +0100

    Setting data now supports all types

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ const { MongoClient } = require(".");
 let db = new MongoClient({
   mongoURI:
     "mongodb+srv://Salvage:SalvageDev@cluster0-bsjyv.mongodb.net/Data?retryWrites=true&w=majority",
-  logFile: "./log.txt",
+  
 });
 (async () => {
-  await db.addSchema("hello");
-  await db.addSchema("TEST_1_2_3");
-  console.log(db.schemas);
-  await db.set("key", "value", "TEST_1_2_3");
-  await db.delete("key", "hello");
-  await db.set("key1", "value1");
-  console.log(await db.get("key1"));
+  await db.addSchema("TEST__");
+  await db.set("TEST2",{
+    Test: "Hi!"
+  },"TEST__");
+  console.log(await db.get("TEST2","TEST__"));
+  await db.set("TEST3",["hi","hello","ski"]);
+  console.log(await db.get("TEST3"))
 })();
