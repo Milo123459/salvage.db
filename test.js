e0a0d72578c4e23cf6690ca997038a64dc15487f
Milo123459 <milo5huntington@gmail.com>
2020-06-08 21:13:11 +0100

    New update

--- test.js
+++ test.js
@@ -4,15 +4,9 @@ let db = new MongoClient({
     "mongodb+srv://Salvage:SalvageDev@cluster0-bsjyv.mongodb.net/Data?retryWrites=true&w=majority",
 });
 (async () => {
-  await db.addSchema("TEST__");
-  await db.set(
-    "TEST2",
-    {
-      Test: "Hi!",
-    },
-    "TEST__"
-  );
-  console.log(await db.get("TEST2", "TEST__"));
-  await db.set("TEST3", ["hi", "hello", "ski"]);
-  console.log(await db.get("TEST3"));
+  await db.set("LolTest",{
+    O: 'L  o l'
+  });
+  console.log(await db.get("LolTest"))
+  console.log(await db.get("LolEJEwqiehqwuh"))
 })();
