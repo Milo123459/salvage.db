4ba9d9a3c5b76917751a76b68d8b0efa7d755e25
Milo123459 <milo5huntington@gmail.com>
2020-06-19 09:44:47 +0100

    New update

--- lakefile.js
+++
@@ -1,5 +0,0 @@
-const { task, series, run, utils } = require("lakesjs");
-const prettier = task("prettier", "Prettify code", () => {
-  require("child_process").execSync("prettier --write .");
-});
-series(prettier)
