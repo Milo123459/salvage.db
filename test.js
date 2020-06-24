67714991a5bbd38384fda22725b81457c28afa1c
Milo123459 <milo5huntington@gmail.com>
2020-06-24 16:13:42 +0100

    Removed bad files

--- test.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient } = require(".");
-let db = new MongoClient({
-  mongoURI:
-    "mongodb+srv://Salvage:SalvageDev@cluster0-bsjyv.mongodb.net/Data?retryWrites=true&w=majority",
-});
-;(
-  async() => {
-  }
-) ();
